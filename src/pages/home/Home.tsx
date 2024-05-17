6a66aa944db82bcb461ed8357e2ae59796bc85f2
Salar Hagh <sallar.hagh@gmail.com>
2024-05-17 15:58:33 +0330

    implementation of footer component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Container from "../../components/container/Container";
+import Footer from "../../components/footer/Footer";
 
 function Home() {
   return (
     <div>
       <Container>
         <img className="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREXlEhCM6HC-rIalmhuCc5BSgDw_UYekfl3tvNICmnRH7JKAP_l9d3vPsNoGGw0iy8ub4&usqp=CAU" alt="" />
-
+        <Footer />
       </Container>
     </div>
   );
