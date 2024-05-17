fbec39c72e9fd967499ee3ec1feeeff34814b42d
Salar Hagh <sallar.hagh@gmail.com>
2024-05-17 15:57:28 +0330

    content of landing

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,10 @@ import Container from "../../components/container/Container";
 function Home() {
   return (
     <div>
-      <Container>Home</Container>
+      <Container>
+        <img className="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREXlEhCM6HC-rIalmhuCc5BSgDw_UYekfl3tvNICmnRH7JKAP_l9d3vPsNoGGw0iy8ub4&usqp=CAU" alt="" />
+
+      </Container>
     </div>
   );
 }
